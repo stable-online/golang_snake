345911888792b2ac98ed365fb73d40fd45788f58
zw <2426595849@qq.com>
2022-07-16 00:03:33 +0800

    [snake] store track

--- game/helper.go
+++ game/helper.go
@@ -1,12 +1,27 @@
 package game
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //全局捕获
 func catch() {
 	if err := recover(); err != nil {
-		//记录日志等操作
+
+		//打印异常
 		fmt.Println(fmt.Sprintf("Catch faild:%s", err))
+
+		//获取堆栈信息(暂时先不获取吧.)
+		/*buf := new(bytes.Buffer)
+		fmt.Fprintf(buf, "%v\n", err)
+		for i := 1; ; i++ {
+			pc, file, line, ok := runtime.Caller(i)
+			if !ok {
+				break
+			}
+			fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		}
+		fmt.Println(buf.String())*/
 	}
 }
 
