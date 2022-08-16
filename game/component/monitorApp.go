29da39fee79ebc95c90f29e422d2153fe119008c
zw <2426595849@qq.com>
2022-08-16 23:37:45 +0800

    [snake] limit struct attribute role

--- game/component/monitorApp.go
+++ game/component/monitorApp.go
@@ -1,15 +1,15 @@
 package component
 
 type monitorApp struct {
-	Monitor monitorFunType
+	monitor monitorFunType
 }
 
 //newMonitorApp 初始化监控应用
 func newMonitorApp() *monitorApp {
-	return &monitorApp{Monitor: initMonitor()}
+	return &monitorApp{monitor: initMonitor()}
 }
 
 //handle 启动监听
 func (s *monitorApp) handle(game *game) {
-	s.Monitor(game.getControl().getMoveChan(), game.getControl().getQuitChan())
+	s.monitor(game.getControl().getMoveChan(), game.getControl().getQuitChan())
 }
--- game/component/screenApp.go
+++ game/component/screenApp.go
@@ -2,15 +2,15 @@ package component
 
 //screenApp 屏幕应用
 type screenApp struct {
-	Screen screenFunType
+	screen screenFunType
 }
 
 //newScreenApp 初始化屏幕应用
 func newScreenApp() *screenApp {
-	return &screenApp{Screen: initScreenHandle()}
+	return &screenApp{screen: initScreenHandle()}
 }
 
 //start 启动屏幕渲染
 func (s *screenApp) start(gameData *game) error {
-	return s.Screen(gameData)
+	return s.screen(gameData)
 }
